eb76c82e782a26acb5d710dad72e5bc57351126d
ivan <ivanchernetskey@mail.ru>
2023-10-27 12:38:50 +0300

    feat: added axios, work with network requests and lifecycle methods

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@
         <post-list
             :posts="posts"
             @remove="removePost"
+            v-if="!isPostsLoading"
         />
+        <div v-else>
+            Loading...
+        </div>
         <my-dialog v-model:show="dialogVisible">
             <post-form 
                 @create="createPost"
@@ -24,6 +28,7 @@
 <script>
 import PostForm from './components/postForm.vue';
 import PostList from './components/PostList.vue';
+import axios from 'axios';
 // здесь мы описываем логику компонента 
 // в этой секции по дефолту нужно экспортировать объект
 
@@ -34,13 +39,10 @@ export default {
     },
     data() {
         return {
-            posts: [
-                {id: 1, title: 'JavaScript', body: 'post description'},
-                {id: 2, title: 'JavaScript2', body: 'post description2'},
-                {id: 3, title: 'JavaScript3', body: 'post description3'},
-                {id: 4, title: 'JavaScript4', body: 'post description4'},
-            ],
-            dialogVisible: false
+            posts: [],
+            dialogVisible: false,
+            modificatorInput: '',
+            isPostsLoading: false
         }
     },
     methods: {
@@ -53,7 +55,21 @@ export default {
         },
         showDialog() {
             this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                this.isPostsLoading = true;
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                this.posts = response.data;
+            } catch(e) {
+                alert(e);
+            } finally {
+                this.isPostsLoading = false;
+            }
         }
+    },
+    mounted() {
+        this.fetchPosts();
     }
 }
 </script>
